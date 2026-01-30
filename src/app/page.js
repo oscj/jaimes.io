a57375b5e3c05f6936be550336f6cea820aae2a6
Oscar Jaimes <oscar@jaimes.io>
2026-01-30 12:02:59 -0700

    simplify

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,14 @@
-import Nav from '@/components/Nav';
-import About from '@/components/About';
-import Projects from '@/components/Projects';
-import Resume from '@/components/Resume';
-
 export default function Home() {
   return (
-    <div className="bg-white h-screen">
-      <Nav />
-      <div className="bg-white text-black pt-16">
-        <section id="about">
-          <br />
-          <br />
-          <br /><About /></section>
-        <section id="projects">
-          <br />
-          <br />
-          <br /><Projects /></section>
-        <section id="resume">
-          <br />
-          <br />
-          <br /><Resume /></section>
+    <div className="landing">
+      <div className="glow" />
+      <div className="content">
+        <h1 className="title">
+          jaimes<span className="dot">.</span>io
+        </h1>
+        <div className="divider" />
+        <p className="tagline">systems & software</p>
       </div>
     </div>
   );
-};
+}
